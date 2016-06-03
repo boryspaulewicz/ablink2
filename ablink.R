0a5d9f81f5cad3e2159eb6241c7d0d22b9d78b1e
Borysław Paulewicz <borys@localhost.localdomain>
2016-06-03 10:27:09 +0200

    dodalem zapis danych kwestionariuszowych

--- ablink.R
+++ ablink.R
@@ -171,13 +171,14 @@ gui.show.instruction("W czasie eksperymentu obowiązuje cisza. Proszę wyłączy
 Za chwilę trzeba będzie wpisać dane osobowe: wiek, płeć oraz pseudonim. Pseudonim składa się z inicjałów (małymi literami) oraz czterech cyfr: dnia i miesiąca urodzenia (np.  ms0706).")
 gui.user.data()
 
+if(USER.DATA$name != 'admin'){
 gui.show.instruction("Teraz rozpocznie się etap polegający na wypełnieniu kilku kwestionariuszy. W każdym z kwestionariuszy prosimy zapoznać się z instrukcją.")
 
 ## PANAS-C
 
 gui.show.instruction('Skala, która się za chwilę pojawi, składa się ze słów nazywających różne emocje i uczucia. Przeczytaj każde słowo i zastanów się jak się czujesz ZAZWYCZAJ.')
 
-gui.quest(c('aktywny(a)', '"jak na szpilkach"', 'mocny(a)', 'nerwowy(a)', 'ożywiony(a)', 'pełen (pełna) zapału', 'przerażony(a)', 'raźny(a)', 'silny(a)', 'winny(a)',
+panas = gui.quest(c('aktywny(a)', '"jak na szpilkach"', 'mocny(a)', 'nerwowy(a)', 'ożywiony(a)', 'pełen (pełna) zapału', 'przerażony(a)', 'raźny(a)', 'silny(a)', 'winny(a)',
             'wystraszony(a)', 'zalękniony(a)', 'zaniepokojony(a)', 'zapalony(a)', 'zawstydzony(a)', 'zdecydowany(a)', 'zdenerwowany(a)', 'zmartwiony(a)', 'żwawy(a)', 'żywy(a)'),
           c('nieznacznie lub wcale', 'trochę', 'umiarkowanie', 'dość mocno', 'bardzo silnie'))
 
@@ -187,7 +188,7 @@ gui.show.instruction(
     'Za chwilę pojawi się oto opis szeregu myśli i uczuć, jakimi ludzie reagują na różne sytuacje i zdarzenia. Prosimy zaznaczyć jak często Pana/Panią nachodzi każda z tych myśli lub uczuć - niezależnie od tego, czy same zdarzenia wywołujące te myśli są rzadkie czy częste: interesuje nas częstość samych myśli lub uczuć, a nie częstość zdarzeń, które je wywołują.
 ')
 
-gui.quest(c(
+ruminacje = gui.quest(c(
 
     'Nie mogę uwolnić się od myśli o różnych niegodziwościach naszego świata.
 ',
@@ -218,7 +219,7 @@ gui.show.instruction(
     'Proszę zaznaczyć stwierdzenie, które najlepiej opisuje jak często czuł(a) się Pan/Pani lub zachowywał(a) w ten sposób w ciągu ostatniego tygodnia.
 ')
 
-gui.quest(c('Martwiły mnie rzeczy, które zazwyczaj mnie nie martwią.',
+cesd = gui.quest(c('Martwiły mnie rzeczy, które zazwyczaj mnie nie martwią.',
             'Nie chciało mi się jeść, nie miałem(am) apetytu.',
             'Czułem(am), że nie mogę pozbyć się chandry, smutku, nawet z pomocą rodziny i przyjaciół.',
             'Wydawało mi się, że jestem gorszym człowiekiem niż inni ludzie.',
@@ -244,7 +245,7 @@ gui.quest(c('Martwiły mnie rzeczy, które zazwyczaj mnie nie martwią.',
 
 gui.show.instruction('Prosimy o ustosunkowanie się do podanych poniżej stwierdzeń zakreślając odpowiedź, która najlepiej opisuje Pana/Pani zachowania na co dzień. Przy każdej z pozycji testowych należy  wybrać jedną z odpowiedzi.')
 
-gui.quest(c(
+sku = gui.quest(c(
     'Nie mogę skoncentrować się na trudnym zadaniu, kiedy wokół mnie jest hałas.',
     'Jest mi trudno skupić uwagę, kiedy muszę się skoncentrować i rozwiązać jakiś problem.',
     'To co dzieje się wokół rozprasza mnie, nawet jeśli pracuję nad czymś intensywnie.',
@@ -275,7 +276,7 @@ Jeżeli w okresie ostatnich 3 miesięcy zdarzyło Ci się coś takiego, zaznacz 
 
 Jeżeli wymienione zdarzenie nie miało miejsca nie zaznaczaj nic na skali przy nim. Dla ułatwienia zdarzenia połączono w grupy odpowiadające poszczególnym obszarom życia.")
 
-gui.quest(c('Poważna kłótnia/konflikt w rodzinie',
+wydarzenia = gui.quest(c('Poważna kłótnia/konflikt w rodzinie',
             'Utrata pracy przez rodzica',
             'Rozwód rodziców',
             'Niesprawiedliwe oskarżenie ze strony rodziców',
@@ -321,12 +322,48 @@ gui.quest(c('Poważna kłótnia/konflikt w rodzinie',
             'Porażka w konkursie lub zawodach',
             'Poważne niepowodzenie w ważnej sprawie'),
           paste(1:5), force.all = F, width = .8)
+} ## tylko dla nie-admina
 
 gui.show.instruction("Teraz rozpocznie się zadanie wykrywania słów innego koloru. Zadanie to składa się z serii prób, w trakcie których na ekranie komputera prezentowane są szybko, jedno po drugim, różne słowa. Większość prezentowanych słów ma kolor biały. Dwa spośród tych słów są zielone. Zadanie to polega na napisaniu, po każdej próbie, jakie było pierwsze i jakie było drugie słowo w kolorze zielonym.")
 
 run.trials(trial.code, record.session = T, expand.grid(t1em = c('neg
 ', 'neu', 'pos
 '), t1pos = 3:5, t2lag = c(2, 4, 6), i = 1:5), condition = 'default')
+
+## Zapisujemy dane kwestionariuszowe
+if(USER.DATA$name != 'admin'){
+    ## Zapamiętujemy dane kwestionariuszowe
+    panas = as.list(panas)
+    names(panas) = paste('i', 1:length(panas), sep = '')
+    db.create.data.table(panas, 'ablink_panas')
+    panas$session_id = SESSION.ID
+    db.insert.data(panas, 'ablink_panas')
+    ## ruminacje
+    ruminacje = as.list(ruminacje)
+    names(ruminacje) = paste('i', 1:length(ruminacje), sep = '')
+    db.create.data.table(ruminacje, 'ablink_ruminacje')
+    ruminacje$session_id = SESSION.ID
+    db.insert.data(ruminacje, 'ablink_ruminacje')
+    ## cesd
+    cesd = as.list(cesd)
+    names(cesd) = paste('i', 1:length(cesd), sep = '')
+    db.create.data.table(cesd, 'ablink_cesd')
+    cesd$session_id = SESSION.ID
+    db.insert.data(cesd, 'ablink_cesd')
+    ## sku
+    sku = as.list(sku)
+    names(sku) = paste('i', 1:length(sku), sep = '')
+    db.create.data.table(sku, 'ablink_sku')
+    sku$session_id = SESSION.ID
+    db.insert.data(sku, 'ablink_sku')
+    ## wydarzenia
+    wydarzenia = as.list(wydarzenia)
+    names(wydarzenia) = paste('i', 1:length(wydarzenia), sep = '')
+    db.create.data.table(wydarzenia, 'ablink_wydarzenia')
+    wydarzenia$session_id = SESSION.ID
+    db.insert.data(wydarzenia, 'ablink_wydarzenia')
+}
+
 
 ## Dalszy etap procedury
 download.run.task("mcmtest")
